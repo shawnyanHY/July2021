3efbb6ce7cb983fb04e41d412d7748bea8e092f2
Abhilash <abhilash.gaddam@antra.com>
2021-08-09 18:41:25 -0400

    Initial Code

---
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieShopMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequestModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserRegisterRequestModel model)
+        {
+            return View();
+        }
+
+    }
+}
